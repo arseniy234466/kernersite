781c71e71dc0fe918a835eefaa7fba7ff73b5509
Арсений <zademidko.senya@yandex.ru>
2021-10-07 01:01:47 +0300

    commit

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ $(window).on('load', function () {
     
 });
 
+function Input(){
+    login_ok = false;
+    user_name = "";
+    password = "";
+    user_name = prompt("Логин","");
+    user_name = user_name.toLowerCase();
+    password = prompt("Пароль","");
+    password = password.toLowerCase();
+    if (user_name=="login" && password=="pass") {
+        login_ok = true;
+        window.location = "index.php";
+    }
+    if (user_name=="login2" && password=="pass2") {
+        login_ok = true;
+        window.location = "forum/index.php";
+    }
+
+    if (login_ok==false) alert("Неверный логин или пароль!");
+}
