00ee13dc29292f12ab88226b78422e8f1027d3f4
Арсений <zademidko.senya@yandex.ru>
2021-10-07 00:12:31 +0300

    Все файлы проекта

---
+++ main.js
@@ -0,0 +1,10 @@
+
+window.onload = function () {
+    document.querySelector('.preloader-2').add("preloader-2-remove");
+};
+
+$(window).on('load', function () {
+    $('.preloader-2').addClass("preloader-2-remove");
+    
+});
+
